ebda905849adac53b952009c07655124c50de931
Sean DiGirolamo <tug70176@temple.edu>
2019-10-10 19:55:45 -0400

    Added documentation comments for the remaining interfaces

--- src/edu/temple/capstone/BinBotServer/AppConnection.java
+++ src/edu/temple/capstone/BinBotServer/AppConnection.java
@@ -1,10 +1,46 @@
 package edu.temple.capstone.BinBotServer;
 
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public interface AppConnection
 {
+	/**
+	 * <h1>sendToApp(InputStream in)</h1>
+	 * This method takes as input an InputStream which will be sent over the socket to whatever client is connected to
+	 * it, in this case the mobile application
+	 *
+	 *
+	 *
+	 * @author  Sean DiGirolamo
+	 * @version 1.0
+	 * @since   2019-10-11
+	 */
 	void sendToApp(InputStream in);
-	InputStream recieve();
-	void listen();
+
+	/**
+	 * <h1>recieve()</h1>
+	 * This method instructs the server to wait to recieve a bitstream from the client. This bitstream will be returned
+	 * to the caller as an outputStream
+	 *
+	 *
+	 *
+	 * @author  Sean DiGirolamo
+	 * @version 1.0
+	 * @since   2019-10-11
+	 */
+	OutputStream recieve();
+
+	/**
+	 * <h1>initiate()</h1>
+	 * This method should be the very first method called by this object. It initiates the socket connection, and doesn't
+	 * complete until a client has connected. This must happen before any other methods or data tranfers can complete
+	 *
+	 *
+	 *
+	 * @author  Sean DiGirolamo
+	 * @version 1.0
+	 * @since   2019-10-11
+	 */
+	void initiate();
 }
--- src/edu/temple/capstone/BinBotServer/ArmInstruction.java
+++ src/edu/temple/capstone/BinBotServer/ArmInstruction.java
@@ -4,5 +4,20 @@ import java.util.List;
 
 public interface ArmInstruction
 {
+	/**
+	 * <h1>calcInstructions(double x, double y, double w, double h)</h1>
+	 * This method takes as input, an x coordinate, a y coordinate, a width, and a height, all indicating the location
+	 * of an identified object in a matrix. Using these values, it will estimate a distance between the camera and the
+	 * object, and the angle that the camera is viewing the object at. The function will then generate a list of
+	 * movements that should be applied to the arm's joins, such that the arm will be in a position where it would grab
+	 * the object were it to squeeze. These instructions will be in the form of a list of doubles, each double
+	 * indicating the angle their corresponding joint should rotate. This list is returned to the caller.
+	 *
+	 *
+	 *
+	 * @author  Sean DiGirolamo
+	 * @version 1.0
+	 * @since   2019-10-09
+	 */
 	List<Double> calcInstructions(double x, double y, double w, double h);
 }
