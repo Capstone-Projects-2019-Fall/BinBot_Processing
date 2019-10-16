8072092f78c1618b5db488a1e1232c94a464202d
Sean DiGirolamo <tug70176@temple.edu>
2019-10-16 11:33:15 -0400

    Implementation of BotConnectionTest

---
+++ test/edu/temple/capstone/BinBotServer/connections/BotConnectionTest.java
@@ -0,0 +1,27 @@
+package edu.temple.capstone.BinBotServer.connections;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BotConnectionTest
+{
+	private static final String JSON_TEST = "{\"message\":\"Hello world!\"}";
+	private static BotConnection botConnection = null;
+
+	@BeforeAll
+	static void construct() throws IOException {
+		botConnection = new BotConnection(7001);
+	}
+
+	@Test
+	static void test() throws IOException {
+		botConnection.accept();
+		botConnection.sendToBot(JSON_TEST);
+		String recieved = botConnection.recieve();
+		assertEquals(JSON_TEST, recieved);
+	}
+}
