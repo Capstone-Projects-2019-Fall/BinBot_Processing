da511b4a38a7fb0c453f3cea0974cfeda7275be6
Sean DiGirolamo <tug70176@temple.edu>
2019-10-29 11:15:22 -0400

    PairTest

---
+++ test/edu/temple/capstone/BinBotServer/util/PairTest.java
@@ -0,0 +1,29 @@
+package edu.temple.capstone.BinBotServer.util;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PairTest
+{
+	static final double KEY = 0.0;
+	static final double VALUE = 1.1;
+
+	static Pair<Double, Double> pair;
+
+	@BeforeAll
+	static void constructor() {
+		pair = new Pair<>(KEY, VALUE);
+	}
+
+	@Test
+	void key() {
+		assertEquals(KEY, pair.key());
+	}
+
+	@Test
+	void value() {
+		assertEquals(VALUE, pair.value());
+	}
+}
