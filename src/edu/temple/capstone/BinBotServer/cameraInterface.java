cf01be50a17f16273988eae48018b5861051437c
Jose Silva <tuf06643@temple.edu>
2019-10-30 10:22:43 -0400

    Created Camera Interface

---
+++ src/edu/temple/capstone/BinBotServer/cameraInterface.java
@@ -0,0 +1,14 @@
+package edu.temple.capstone.BinBotServer;
+
+import java.util.*;
+
+
+/**
+ *  Camera Interface that will contain methods:
+ *      capture_photo()
+ *      get_proximity()
+ *      in_range(): bool
+ */
+
+public interface cameraInterface {
+}
